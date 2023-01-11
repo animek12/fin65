887917fd11f47e46d7386378741f5b1db92a8264
animek12 <111511065+animek12@users.noreply.github.com>
2023-01-11 22:21:43 +0800

    Update groupbot.js

--- plugins/groupbot.js
+++ plugins/groupbot.js
@@ -6,7 +6,7 @@ const sections = [
    {
 	title: `𝗝𝗢𝗜𝗡 𝗞𝗘 𝗚𝗥𝗨𝗣 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗕𝗢𝗧*\n\n\n━━━〔 Dᴏɴ'ᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕━━━`,
 	rows: [
-	    {title: '💌 › Group NexBotz', description: "Group Utama putra NexBotz", rowId:".gcbot"}
+	    {title: '💌 › Group ꜰɪɴ ᴏꜰꜰɪᴄɪᴀʟ', description: "Group Utama putra ꜰɪɴ", rowId:".gcbot"}
 	    ]
       },
 ]
